37a0241a218576d3f75305a689c4e2c1e12b06d9
hdpusr <hdpusr@sandbox.bigdata.lab>
2016-05-21 18:02:11 -0400

    use wget

--- rsync-puppetlab.sh
+++ rsync-puppetlab.sh
@@ -1,1 +1,2 @@
-rsync -avSHP --delete --include "*/1.1[0,1]/el[6,7]" --exclude "*i386*" --exclude "SRPMS" rsync://yum.puppetlabs.com/yum/el/[6,7] /opt/yum/puppetlabs
+wget -X i386,SRPMS -r -np -k -A rpm http://yum.puppetlabs.com/el/7/products/x86_64
+
