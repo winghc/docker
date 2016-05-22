23307084295c3ed63ed683587884da5752260c98
hdpusr <hdpusr@sandbox.bigdata.lab>
2016-05-22 00:29:16 -0400

    using wget

--- rsync-foreman.sh
+++ rsync-foreman.sh
@@ -1,1 +1,2 @@
-rsync -avSHP --delete --include "el6" --include "el7"  --exclude "*i386*" --exclude "source" rsync://yum.theforeman.org/yum/releases/1.10 /opt/repos/foreman/1.10
+#rsync -avSHP --delete --include "el7"  --exclude "*i386*" --exclude "source" rsync://yum.theforeman.org/yum/releases/1.11 /opt/repos/foreman/1.11
+wget -X i386,SRPMS -r -np -k -A rpm http://yum.theforeman.org/releases/1.11/el7/x86_64/
--- rsync-puppetlab.sh
+++ rsync-puppetlab.sh
@@ -1,2 +1,3 @@
-wget -X i386,SRPMS -r -np -k -A rpm http://yum.puppetlabs.com/el/7/products/x86_64
+wget -X i386,SRPMS -N -r -np -k -A rpm http://yum.puppetlabs.com/el/7/products/x86_64
+wget -X i386,SRPMS -N -r -np -k -A rpm http://yum.puppetlabs.com/el/6/products/x86_64
 
