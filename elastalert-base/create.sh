02a48f209dc88f6c4366a25042fb9a8b2460cf2e
hu chen <huchen_kzx@citicbank.com>
2016-11-10 00:02:59 +0800

    add base image of python

---
+++ elastalert-base/create.sh
@@ -0,0 +1,2 @@
+docker rmi citic/elastalert-base:0.0.95
+docker build -t citic/elastalert-base:0.0.95 .
--- elastalert/create.sh
+++ elastalert/create.sh
@@ -1,2 +1,2 @@
-docker rmi citic/elastalert:0.1.1
-docker build -t citic/elastalert:0.1.1 .
+docker rmi citic/elastalert:0.0.95
+docker build -t citic/elastalert:0.0.95 .
